845f17a0342701039fb396ffc3bb08f7ee54bdff
Fabio Masciotra <fabmas@microsoft.com>
2023-11-06 21:23:52 +0100

    update

--- .scripts/ExecuteScript.ps1
+++ .scripts/ExecuteScript.ps1
@@ -20,9 +20,13 @@ Param
  
  )
 
+ if ($DomainAdminName -like "*@*") {
+    $parts = $DomainAdminName -split "@"
+    $DomainAdminName = $parts[1] + "\" + $parts[0]
+}
+
 $SecureString = ConvertTo-SecureString -AsPlainText $DomainAdminPWD -Force
 $SecureCreds = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $DomainAdminName,$SecureString 
-
 
 # create a local folder named c:\temp\script and copy the "https://raw.githubusercontent.com/fabmas/certlc/main/.scripts/InstallEntRootCA.ps1" file into it
 $ScriptFolder="c:\temp\script"
@@ -33,7 +37,5 @@ $ScriptURL="https://raw.githubusercontent.com/fabmas/certlc/main/.scripts/Instal
 Invoke-WebRequest -uri $ScriptURL -OutFile $ScriptPath
 
 # then run the following command to execute the script
-# Invoke-Command -FilePath $ScriptPath -ComputerName $env:COMPUTERNAME -Credential $SecureCreds -Authentication Negotiate -EnableNetworkAccess -ArgumentList $CAName,$CDPURL,$WebenrollURL,$demoCertDNSName -Verbose
-
 Start-Process -FilePath "powershell.exe " -Credential $SecureCreds -ArgumentList "-File $ScriptPath $CAName $CDPURL $WebenrollURL $demoCertDNSName" -Verbose
 
