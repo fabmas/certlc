2458941462e676b57e733e02f946939f6ebdffa7
Fabio Masciotra <fabmas@microsoft.com>
2023-11-08 23:05:38 +0100

    restored

--- .armtemplate/DSC/ExecuteScript.ps1
+++ .armtemplate/DSC/ExecuteScript.ps1
@@ -30,7 +30,7 @@ configuration ExecuteScript
         [Int]$RetryCount=20,
         [Int]$RetryIntervalSec=30
     )
-    Import-DscResource -ModuleName xComputerManagement, xActiveDirectory,PSDesiredStateConfiguration, PackageManagement
+    Import-DscResource -ModuleName PSDesiredStateConfiguration, PackageManagement
     [System.Management.Automation.PSCredential ]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${DomainName}\$($Admincreds.UserName)", $Admincreds.Password)
 
     Node localhost
@@ -57,23 +57,6 @@ configuration ExecuteScript
         {
             Name        = "RSAT-AD-PowerShell"
             Ensure      = "Present"
-        }
-
-        xWaitForADDomain DscForestWait 
-        { 
-            DomainName           = $DomainName 
-            DomainUserCredential = $DomainCreds
-            RetryCount           = $RetryCount 
-            RetryIntervalSec     = $RetryIntervalSec 
-            DependsOn            = "[WindowsFeature]ADPS"
-        }
-        
-        xComputer DomainJoin
-        {
-            Name        = $env:COMPUTERNAME
-            DomainName  = $DomainName
-            Credential  = $DomainCreds
-            DependsOn   = "[xWaitForADDomain]DscForestWait"
         }
 
         script 'ExecuteScript'
