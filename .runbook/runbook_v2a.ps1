af68a0558d04ab522f94db976a4119c4a44dee25
Fabio Masciotra <fabmas@microsoft.com>
2023-11-14 22:41:24 +0100

    risolto problema TAG sul certificato

--- .runbook/runbook_v2a.ps1
+++ .runbook/runbook_v2a.ps1
@@ -124,52 +124,52 @@ try {
         Write-Output "Recipient: $Recipient"
         # Import the new certificate in the Key Vault
         try {
-            $tag = @{ "recipient" = $Recipient  }
-            Import-AzKeyVaultCertificate -VaultName $VaultName -Name $ObjectName -FilePath $tempFile
-            Update-AzKeyVaultCertificate -VaultName $VaultName -Name $ObjectName -Tags $tag -PassThru
+            $tag =  @{recipient = $Recipient}
+            $newCert = Import-AzKeyVaultCertificate -VaultName $VaultName -Name $ObjectName -FilePath $tempFile 
         } catch {
             Write-Error "Error importing certificate to Key Vault: $_"
             throw
         }
 
         # Send notification email to recipient
-        try {
-
-            #Get Date
-            $MailDate = Get-Date -format "MM-dd-yyyy"
-            
-            #Configuration Variables for E-mail
-            $SmtpServer = "localhost" 
-            $EmailFrom = "Certificate LifeCycle Automation <clc@demo.com>"
-            $EmailTo = $Recipient 
-            $EmailSubject = "Certificate $ObjectName renewed"
-            #HTML Template
-            $EmailBody = @"
-            <table style="width: 68%" style="border-collapse: collapse; border: 1px solid #008080;">
-            <tr>
-                <td colspan="2" bgcolor="#008080" style="color: #FFFFFF; font-size: large; height: 35px;">
-                    Certificate LifeCycle Automation - Certificate Update Notification Maildate
-                </td>
-            </tr>
-            <tr style="border-bottom-style: solid; border-bottom-width: 1px; padding-bottom: 1px">
-                <td style="width: 201px; height: 35px">  Name of Updated Certificate</td>
-                <td style="text-align: center; height: 35px; width: 233px;">
-                <b>CertName</b></td>
-            </tr>
-            </table>
+        if ($null -ne $Recipient){
+            try {
+                
+                $newCert | Update-AzKeyVaultCertificate -Tag $tag
+                #Get Date
+                $MailDate = Get-Date -format "MM-dd-yyyy"
+                
+                #Configuration Variables for E-mail
+                $SmtpServer = "localhost" 
+                $EmailFrom = "Certificate LifeCycle Automation <clc@demo.com>"
+                $EmailTo = $Recipient 
+                $EmailSubject = "Certificate $ObjectName renewed"
+                #HTML Template
+                $EmailBody = @"
+                <table style="width: 68%" style="border-collapse: collapse; border: 1px solid #008080;">
+                <tr>
+                    <td colspan="2" bgcolor="#008080" style="color: #FFFFFF; font-size: large; height: 35px;">
+                        Certificate LifeCycle Automation - Certificate Update Notification Maildate
+                    </td>
+                </tr>
+                <tr style="border-bottom-style: solid; border-bottom-width: 1px; padding-bottom: 1px">
+                    <td style="width: 201px; height: 35px">  Name of Updated Certificate</td>
+                    <td style="text-align: center; height: 35px; width: 233px;">
+                    <b>CertName</b></td>
+                </tr>
+                </table>
 "@
 
-            $EmailBody= $EmailBody.Replace("CertName",$ObjectName)
-            $EmailBody= $EmailBody.Replace("Maildate",$Maildate)
-            
-            #Send E-mail from PowerShell script
-            Send-MailMessage -To $EmailTo -From $EmailFrom -Subject $EmailSubject -Body $EmailBody -BodyAsHtml -SmtpServer $SmtpServer
+                $EmailBody= $EmailBody.Replace("CertName",$ObjectName)
+                $EmailBody= $EmailBody.Replace("Maildate",$Maildate)
+                
+                #Send E-mail from PowerShell script
+                Send-MailMessage -To $EmailTo -From $EmailFrom -Subject $EmailSubject -Body $EmailBody -BodyAsHtml -SmtpServer $SmtpServer
 
-
-
-        } catch {
-            Write-Error "Error sending email notification: $_"
-            throw
+            } catch {
+                Write-Error "Error sending email notification: $_"
+                throw
+            }
         }
 
         # Check if any of the specified variables are null and exit if true
