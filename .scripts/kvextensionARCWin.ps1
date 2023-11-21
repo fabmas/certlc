769db45169c06a4035ef26aa76b198bcc2e0ef51
Angelo Mazzucchi <angelo.mazzucchi@outlook.com>
2023-11-21 13:47:37 +0100

    Arc_Win

--- .scripts/kvextensionARCWin.ps1
+++ .scripts/kvextensionARCWin.ps1
@@ -1,49 +1,41 @@
 # NOTE
-# Remeber to assign to the system managed identity of the ARC Server the "Key Vault Secret User" role on the Key Vault 
+# Remeber to assign to the Service Principal of the ARC Server the "Key Vault Secret User" role on the Key Vault 
 
 # Customizable variables
-$vmName = "VM01"                # Name of the VM where the extension will be deployed
+$vmName = "win01"                # Name of the VM where the extension will be deployed
 $VMresourceGroupName = "certlc" # Resource group where the VM is located
 $location = "westeurope"        # Location of the VM
-$keyVaultName = "KV01"          # Name of the Key Vault holding the certificate
+$keyVaultName = "DEMO-KV-fab01"          # Name of the Key Vault holding the certificate
 $certifcateName = "democert"    # Name of the certificate in the Key Vault
 $pollingInterval = "43200"      # Polling interval in seconds (e.g. 43200 = 12 hours)
 
-
 # Build settings on ARC Windows Server
-$Settings = @'
-{
-   "secretsManagementSettings": {
-        "pollingIntervalInS": "POLLINGPLACEHOLDER",
-        "linkOnRenewal": false,
-        "observedCertificates":
-        [
-            {
-                "url": "https://KVPLACEHOLDER.vault.azure.net:443/secrets/CERTPLACEHOLDER",
-                "certificateStoreName": "MY",
-                "certificateStoreLocation": "LocalMachine",
-                "keyExportable": true,
-                "accounts": [
-                    "Network Service",
-                    "Local Service"
-                ]
-            }
-        ]
-    },
-   "authenticationSettings": {
-    "msiEndpoint": "http://localhost:40342/metadata/identity"
-  }
-}
-'@ 
 
-$Settings = $Settings.Replace("POLLINGPLACEHOLDER",$pollingInterval)
-$Settings = $Settings.Replace("KVPLACEHOLDER",$keyVaultName)
-$Settings = $Settings.Replace("CERTPLACEHOLDER",$certifcateName)
+$Settings = @{
+    secretsManagementSettings = @{
+        pollingIntervalInS = $pollingInterval
+        linkOnRenewal = $false
+        observedCertificates = @(
+            @{
+                url = "https://$keyVaultName.vault.azure.net:443/secrets/$certifcateName"
+                certificateStoreName = "MY"
+                certificateStoreLocation = "LocalMachine"
+                keyExportable = $true
+                accounts = @("Network Service", "Local Service")
+            }
+            # Add more here, don't forget a comma on the preceding line
+        )
+        # The cert store location is optional, the default path is shown below
+        # certificateStoreLocation = "/var/lib/waagent/Microsoft.Azure.KeyVault.Store/"
+    }
+    authenticationSettings = @{
+        msiEndpoint = "http://localhost:40342/metadata/identity"
+    }
+}
 
 $extName = "KeyVaultForWindows"
 $extPublisher = "Microsoft.Azure.KeyVault"
 $extType = "KeyVaultForWindows"
-
 # Start the deployment on Windows ARC Server
 New-AzConnectedMachineExtension -ResourceGroupName $VMresourceGroupName -MachineName $vmName -Name $extName -Location $location -Publisher $extPublisher -ExtensionType $extType -Setting $Settings
 
